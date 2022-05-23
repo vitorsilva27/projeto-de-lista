5adc5f3db45e8a2d8cb31a49e50b9d79e941fa01
Vitor Gabriel <vg285757@gmail.com>
2022-05-23 19:22:12 -0300

    feat: evolving code

---
+++ src/mock/index.tsx
@@ -0,0 +1,30 @@
+export const mockList = [
+  {
+    company: "Facebook",
+    ceo: "Mark Zuckerberg",
+    cfo: "David Wehner",
+    revenue: "85.96 billion USD",
+    founded: "February 2004",
+  },
+  {
+    company: "Amazon",
+    ceo: "Jeff Bezos",
+    cfo: "Andy Jassy",
+    revenue: "386.1 billion USD",
+    founded: "July 1994",
+  },
+  {
+    company: "AliExpress",
+    ceo: "Jack Ma",
+    cfo: "Jack Ma",
+    revenue: "36.1 billion USD",
+    founded: "July 2002",
+  },
+  {
+    company: "Devs",
+    ceo: "Vitor",
+    cfo: "Silva",
+    revenue: "1.1 billion USD",
+    founded: "March 2025",
+  },
+];
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-function Home() {
+  import { Container } from "./style"
+
+import { mockList } from '../../mock';
+
+const Home: React.FC = () => {
   return (
-    <div>
-      oi
-    </div>
+    <Container>
+      <h1>Big Companies</h1>
+      {mockList.map(list => (
+        <div>
+          <p>Company: {list.company}</p>
+          <p>Ceo: {list.ceo}</p>
+          <p>Cfo: {list.cfo}</p>
+          <p>Revenue: {list.revenue}</p>
+          <p>Founded: {list.founded}</p>
+          <hr/>
+        </div>
+      ))}
+    </Container>
   );
 }
 
